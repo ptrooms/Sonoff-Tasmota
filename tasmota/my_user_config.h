96501e857bc5ad722b29a262a8c0f14be13a461e
PtrO <34420738+ptrooms@users.noreply.github.com>
2022-06-16 18:44:20 +0200

    tasmota 9.3.1

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -35,6 +35,8 @@
  * ATTENTION:
  * Users are advised to use the user_config_override.h file for most changes.
 \*********************************************************************************************/
+#define USE_CONFIG_OVERRIDE                      // Uncomment to use user_config_override.h file. See README.md
+
 
 /*********************************************************************************************\
  * SECTION 1
